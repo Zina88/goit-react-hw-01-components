ae91618ab0e4677d069e837045e536dd08c2c37c
Zina <zinasaenko@hotmail.com>
2022-09-11 14:52:39 +0300

    create friendsList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
-import { Statistics } from 'components/Statistics/Statistics';
 import { Profile } from 'components/Profile/Profile';
-import data from 'data/data.json';
 import user from 'data/user.json';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import data from 'data/data.json';
+
+import { FriendList } from 'components/FriendList/FriendList';
+import friends from 'data/friends.json';
 
 export const App = () => {
   return (
@@ -14,6 +18,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <>
+      <ul className={css.friendList}>
+        {friends.map(({ id, avatar, name, isOnline }) => (
+          <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+FriendList.propTypes = {
+  freiends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }),
+  ),
+};
